9170f50d3f060b0e5b6bc4799184cbb82633c7ca
Yaniv Bar <84463327+cookieman912@users.noreply.github.com>
2022-08-02 11:30:22 +0300

    WP Extentsion Fix- Now works only on first item added

--- plugins/theme-customisations-master/custom/functions.php
+++ plugins/theme-customisations-master/custom/functions.php
@@ -17,8 +17,11 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 
 
-add_action( 'woocommerce_add_to_cart', 'echoGreetUser' );
+add_action( 'woocommerce_add_to_cart', 'echoGreetUser',10,6 );
 
-function echoGreetUser() {
-	echo '<h2 class="greet-message">Thank you for adding product to cart!</h2>';
+function echoGreetUser( $cart_id, $product_id, $request_quantity, $variation_id, $variation, $cart_item_data) {
+	global $woocommerce;
+	if ($woocommerce->cart->get_cart_contents_count() == 1) {
+		echo '<h2 class="greet-message">Thank you for adding product to cart!</h2>'; 
+	}
 }
